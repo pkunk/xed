a8630a8ffd0f74ce872d6681d96b36b639aa7673
Alex B <pkunk.ab@gmail.com>
2026-02-17 18:39:11 +0200

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,7 @@ impl eframe::App for XedApp {
             ui.horizontal(|ui| {
                 ui.label("        ");
                 if ui.button("Open").clicked() {
-                    let mut dialog = FileDialog::open_file(Some(PathBuf::from(".")));
+                    let mut dialog = FileDialog::open_file().initial_path(".");
                     dialog.open();
                     self.open_file_dialog = Some(dialog);
                 }
